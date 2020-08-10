245c1de2fe4bf11eca472f59731a4b33cb98fba8
WMXPY <wm@mengw.io>
2020-08-10 11:02:00 -0500

    add open command

--- src/open.ts
+++ src/open.ts
@@ -6,11 +6,22 @@
 
 import { exec } from "child_process";
 
+const getOpenCommand = (): string => {
+
+    switch (process.platform) {
+        case 'win32':
+            return 'start';
+    }
+
+    return 'open';
+};
+
 export const openFile = (file: string): Promise<void> => {
 
     return new Promise<void>((resolve: () => void, reject: (reason: any) => void) => {
 
-        exec(`open ${file}`, (error: any, stdout: string, stderr: string) => {
+        const openCommand: string = getOpenCommand();
+        exec(`${openCommand} ${file}`, (error: any, stdout: string, stderr: string) => {
 
             if (!error) {
                 resolve();
